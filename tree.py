e2ae7726925cd4ee129c8d38b45d0ebaa3db59ec
Richmond Kessie <kessierich.mag@gmail.com>
2022-10-27 10:40:34 +0000

    Christmas Tree

---
+++ tree.py
@@ -0,0 +1,18 @@
+# This is a program displaying matrix
+picture = [
+    [0,0,0,1,0,0,0],
+    [0,0,1,1,1,0,0],
+    [0,1,1,1,1,1,0],
+    [1,1,1,1,1,1,1],
+    [0,0,0,1,0,0,0],
+    [0,0,0,1,0,0,0]
+]
+
+for row in picture:
+    for pixel in row:
+        if pixel == 1:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    # Create a new line after every iteration
+    print('')
